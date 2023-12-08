2529ba5d34b7a006dee79de47cb5c3ca6f70cc10
Ivan Adamov <psnasti@gmail.com>
2023-12-08 14:59:27 +0100

    KNN model added

--- models/regression/linear_regression/linear_regression.py
+++ models/regression/linear_regression/linear_regression.py
@@ -9,21 +9,18 @@ from matplotlib import pyplot as plt
 
 from helpers import helpers
 
-
 new_model_created = False
+
+# the column that we're going to predict
+predict = "G3"
 
 # read the data from csv
 students_data_path = helpers.get_path_in_repo("data", "student-mat.csv")
 full_data = pd.read_csv(students_data_path, sep=";")
 
-# select only certain columns
-selected_data = full_data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
-data_without_g3 = selected_data.drop(columns=["G3"])
-data_only_g3 = selected_data[["G3"]]
-
 # split data into what we're going to feed to the model, and what it must predict
-input_data = np.array(data_without_g3)  # the model will take data without G3 column
-output_data = np.array(data_only_g3)  # the model will predict G3 value
+input_data = full_data[["G1", "G2", "studytime", "failures", "absences"]]  # the model will take data from a few columns
+output_data = full_data[[predict]]  # the model will predict G3 value
 
 # split  x and y: 0.1 to test data, 0.9 to learning data
 # this way, tests are done against data that has not been seen
@@ -74,31 +71,36 @@ linear_model_accuracy = linear_regression_model.score(X=input_data_test, y=outpu
 print(f"Model accuracy: {linear_model_accuracy}")
 
 # output the model coefficients
-data_importance_coefficients = pd.DataFrame(linear_regression_model.coef_, columns=data_without_g3.columns)
+data_importance_coefficients = pd.DataFrame(linear_regression_model.coef_, columns=input_data.columns)
 print(f"Importance coefficients of the input data:")
 print(data_importance_coefficients)
 
 # output the intercept - the point where the linear regression crosses the y-axis
 print(f"Intercept: {linear_regression_model.intercept_}")
 
-# visualize testing the model
+# visualize predictions along with train and test data
 predictions: np.ndarray = linear_regression_model.predict(input_data_test)
+prediction_range = abs(max(output_data_train[predict]) - min(output_data_train[predict]))
 test_model_df = pd.DataFrame([
     {
-        "input_test_data": input_data_test[index],
-        "expected_prediction": output_data_test[index][0],
+        "input_test_data": input_data_test.iloc[index].values,
+        "expected_prediction": output_data_test.iloc[index].iloc[0],
         "actual_prediction": predictions[index][0],
-        "accuracy": int((20 - abs(output_data_test[index][0] - predictions[index][0])) / 20 * 100)
+        "accuracy": int((prediction_range - abs(
+            output_data_test.iloc[index].iloc[0] - predictions[index][0])) / prediction_range * 100)
     }
     for index in range(len(predictions))
 ])
 test_model_df['accuracy'] = test_model_df['accuracy'].apply(helpers.df_cell_red_color)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 print(test_model_df)
 
 # plot the correlation between initial data and G3
 plt.style.use("ggplot")
-n1 = int(math.sqrt(len(data_without_g3.columns)))
-n2 = len(data_without_g3.columns) - n1
+n1 = int(math.sqrt(len(input_data.columns)))
+n2 = len(input_data.columns) - n1
 nrows = min(n1, n2)
 ncols = max(n1, n2)
 fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(22, 10))
@@ -106,10 +108,10 @@ fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(22, 10))
 i = 0
 for row in axes:
     for ax in row:
-        if i < len(data_without_g3.columns):
-            x_axis = data_without_g3.columns[i]
-            y_axis = "G3"
-            ax.scatter(x=selected_data[x_axis], y=selected_data[y_axis])
+        if i < len(input_data.columns):
+            x_axis = input_data.columns[i]
+            y_axis = output_data.columns[0]
+            ax.scatter(x=input_data[x_axis], y=output_data[y_axis])
             ax.set_title(f'Correlation between "{x_axis}" and "{y_axis}" in the initial data')
             ax.set_xlabel(f"{x_axis}: {linear_regression_model.coef_[0][i]}")
             ax.set_ylabel(y_axis)
