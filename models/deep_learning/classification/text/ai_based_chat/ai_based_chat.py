fe659754c51f5a1b8d9c90b3aee9422a33132acf
E1Adamov <psnasti@gmail.com>
2023-12-27 11:09:54 +0100

    improved the chat model accuracy from 0.84 to 1.00 (#10)

--- models/deep_learning/classification/text/ai_based_chat/ai_based_chat.py
+++ models/deep_learning/classification/text/ai_based_chat/ai_based_chat.py
@@ -14,6 +14,8 @@ from helpers import helper
 
 
 LOAD_MODEL = False
+PATTERN_LENGTH_LIMIT = 255
+
 intents_json_path = helper.get_path_in_repo("data", "intents.json")
 
 
@@ -49,16 +51,18 @@ def get_topic_predicting_model() -> Tuple[keras.Sequential, list[str], Dict[int,
                     pattern
                 )  # split by " ", including punctuation
 
-                patterns.append(wrds)
-
                 wrds = [
                     stemmer.stem(w.lower()) for w in wrds if w not in string.punctuation
                 ]
+                patterns.append(wrds)
                 vocabulary.extend(wrds)
 
                 tags_corresponding_to_patterns.append(tag)
 
         vocabulary = sorted(list(set(vocabulary)))  # remove duplicates
+        vocabulary = {
+            word: code for word, code in zip(vocabulary, range(1, len(vocabulary) + 1))
+        }
         unique_labels = sorted(list(unique_labels))
         label_encoder = preprocessing.LabelEncoder()
         encoded_labels = label_encoder.fit_transform(unique_labels)
@@ -75,8 +79,15 @@ def get_topic_predicting_model() -> Tuple[keras.Sequential, list[str], Dict[int,
         # 1 means the word was used, agnostic of how many times it was used
         training: list[list[int]] = []
 
-        for tag_index, pattern in enumerate(patterns):
-            bag_of_words = [1 if word in pattern else 0 for word in vocabulary]
+        longest_pattern = max(len(p) for p in patterns)
+        global PATTERN_LENGTH_LIMIT
+        PATTERN_LENGTH_LIMIT = max(longest_pattern + 20, PATTERN_LENGTH_LIMIT)
+
+        for pattern in patterns:
+            bag_of_words = [vocabulary[word] for word in pattern]
+            bag_of_words = bag_of_words + [0] * (
+                PATTERN_LENGTH_LIMIT - len(bag_of_words)
+            )
             training.append(bag_of_words)
 
         training_data: np.ndarray = np.array(training)
@@ -84,11 +95,11 @@ def get_topic_predicting_model() -> Tuple[keras.Sequential, list[str], Dict[int,
         tag_predicting_model = keras.Sequential(
             layers=[
                 keras.layers.Dense(
-                    units=8,
-                    input_shape=(len(vocabulary),),
+                    units=PATTERN_LENGTH_LIMIT,
+                    input_shape=(PATTERN_LENGTH_LIMIT,),
                     activation="relu",
                 ),  # Input layer
-                keras.layers.Dense(units=128, activation="relu"),  # Hidden layer
+                keras.layers.Dense(units=16, activation="relu"),  # Hidden layer
                 keras.layers.Dense(
                     units=len(unique_labels), activation="softmax"
                 ),  # Output layer
@@ -104,10 +115,9 @@ def get_topic_predicting_model() -> Tuple[keras.Sequential, list[str], Dict[int,
         tag_predicting_model.fit(
             x=training_data,
             y=encoded_labels_corresponding_to_patterns,
-            epochs=100,
-            batch_size=32,
+            epochs=350,
+            batch_size=8,
             verbose=1,
-            validation_split=0.2,
         )
         with open(model_path, "wb") as f:
             pickle.dump((tag_predicting_model, vocabulary, labels_map), f)
@@ -127,8 +137,10 @@ def chat():
         if inp == "q":
             break
 
-        bag_of_words = helper.bag_of_words(sentence=inp, vocabulary=vocabulary)
-        prediction = model.predict([bag_of_words])
+        bag_of_words = helper.bag_of_words(
+            sentence=inp, vocabulary=vocabulary, max_length=PATTERN_LENGTH_LIMIT
+        )
+        prediction = model.predict(bag_of_words)
         index_of_the_predicted_label = np.argmax(prediction[0])
         predicted_label = labels_map[index_of_the_predicted_label]
 
@@ -137,7 +149,7 @@ def chat():
             if tag == predicted_label:
                 possible_answers = intent["responses"]
                 response = random.choice(possible_answers)
-                print(response)
+                print(tag, response)
 
 
 if __name__ == "__main__":
